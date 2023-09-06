bee8267f00cd5a7352e874acd4e7cd8e9559de1d
Helen Esterman <helenesterman99@gmail.com>
2023-09-06 15:41:25 -0400

    added routing to customer details and created components for customer details

--- src/services/employeeService.js
+++ src/services/employeeService.js
@@ -5,11 +5,11 @@ export const getAllEmployees = () => {
 };
 
 // this is only used when trying to fine assigned employee through the employee ID
-// export const getEmployeeById = (employeeId) => {
-//   return fetch(
-//     `http://localhost:8088/employees/${employeeId}?_expand=user`
-//   ).then((res) => res.json());
-// };
+export const getEmployeeById = (employeeId) => {
+  return fetch(
+    `http://localhost:8088/employees/${employeeId}?_expand=user`
+  ).then((res) => res.json());
+};
 
 export const getAllEmployeesInfo = () => {
   return fetch("http://localhost:8088/users?isStaff=true").then((res) =>
